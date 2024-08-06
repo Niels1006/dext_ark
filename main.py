5cb2d9d419f2450d7372d7ca309ffcf734b8435d
Niels <hermsen.niels@gmail.com>
2024-08-06 20:21:17 +0200

    reqs

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import time
 
 import pyautogui
 
-SIDEWALK_TIME = 0.16
+SIDEWALK_TIME = 0.15
 SPARK_POWDER = (1381, 371)
 GUN_POWDER = (1566, 370)
 PULL_BUTTON = (1440, 36)
@@ -23,15 +23,16 @@ def close_remote_inv():
 
 
 def craft():
-    pyautogui.click(SPARK_POWDER)
-    time.sleep(0.2)
-    pyautogui.click(PULL_BUTTON)
-    time.sleep(0.2)
-    pyautogui.click(SPARK_POWDER)
-    time.sleep(0.2)
-    for _ in range(10):
-        pyautogui.press('a')
-        time.sleep(0.1)
+    for item in [SPARK_POWDER, GUN_POWDER]:
+        pyautogui.click(item)
+        time.sleep(0.2)
+        pyautogui.click(PULL_BUTTON)
+        time.sleep(0.2)
+        pyautogui.click(item)
+        time.sleep(0.2)
+        for _ in range(10):
+            pyautogui.press('a')
+            time.sleep(0.1)
 
 
 def main():
