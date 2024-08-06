708f86f0a30390f128eb1328a3c1014f7f60223c
Niels <hermsen.niels@gmail.com>
2024-08-06 20:08:10 +0200

    reqs

--- main.py
+++ main.py
@@ -2,38 +2,41 @@ import time
 
 import pyautogui
 
+SIDEWALK_TIME = 0.3
+SPARK_POWDER = (1381, 371)
+GUN_POWDER = (1566, 370)
+PULL_BUTTON = (1440, 36)
 
-# SIDEWALK_TIME = 1
-#
-#
-# def walk():
-#     pyautogui.keyDown('w')
-#     time.sleep(SIDEWALK_TIME)
-#     pyautogui.keyUp('w')
-#
-# def access_remote_inv():
-#     pyautogui.keyDown('f')
-#     time.sleep(1)
-#     pyautogui.keyUp('f')
-#
-#
-# if __name__ == '__main__':
-#     walk()
 
-def place():
-    pyautogui.click(100, 100)
-    pyautogui.click(100, 100)
+def walk():
+    pyautogui.keyDown('w')
+    time.sleep(SIDEWALK_TIME)
+    pyautogui.keyUp('w')
+
+
+def access_remote_inv():
+    pyautogui.keyDown('f')
+    time.sleep(1)
+    pyautogui.keyUp('f')
+
+
+def craft():
+    pyautogui.click(SPARK_POWDER)
+    time.sleep(0.2)
+    pyautogui.click(PULL_BUTTON)
+    time.sleep(0.2)
+    for _ in range(10):
+        pyautogui.keyDown('a')
+        time.sleep(0.2)
+        pyautogui.keyUp('a')
 
 
 def main():
-    pyautogui.keyDown('s')
-
-    for _ in range(100):
-        place()
-
-    pyautogui.keyUp('s')
+    for _ in range(4):
+        access_remote_inv()
+        craft()
+        walk()
 
 
 if __name__ == '__main__':
-    time.sleep(10)
     main()
